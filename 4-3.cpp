82f388c6cf793f624add8a1e6a544e8f8bd1d351
rhakash008 <rhakash008@gmail.com>
2024-11-27 11:15:33 +0600

    Create 4-3.cpp

---
+++ 4-3.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+class node
+{
+  public:
+  int data;
+  node* next;
+
+    node(int val)
+  {
+      data=val;
+      next=NULL;
+  }
+};
+
+void insert(node* &head,int val){
+    node* n=new node(val);
+        if(head==NULL){
+        head=n;
+        return;
+    }
+    node* temp= head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+void newinsert(node* &head,int val)
+{
+    node* n = new node(val);
+    node* temp=head;
+    while(temp->data!=4){
+        temp=temp->next;
+    }
+    n->next=temp->next;
+    temp->next=n;
+}
+
+void show(node* head)
+{
+    node* temp = head;
+    while(temp != NULL){
+        cout << temp->data;
+        if (temp->next != NULL) { 
+            cout << "->";
+        }
+        temp = temp->next;
+    }
+    cout <<"->NULL"<<endl;
+}
+
+int main()
+{
+    node* head=NULL;
+    int num;
+    for(int i=0;i<5;i++){
+        cin>>num;
+        insert(head,num);
+    }
+    cout<<"Main link list: ";
+    show(head);
+    newinsert(head,7);
+    cout<<"\nNew: ";
+    show(head);
+    return 0;
+}
